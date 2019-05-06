43b9e56a4916f44c16b3a9587844c051a46c9a1a
hugo <sirso.fell@protonmail.com>
2019-05-06 17:16:10 +0200

    adding sound and exception for items

--- item.py
+++ item.py
@@ -22,22 +22,25 @@ class Items(object):
         self.x_tube = self.tile_x_tube * sprite_size
         self.tile_y_tube = 0
         self.y_tube = self.tile_y_tube * sprite_size
+        self.forbiden_tulpes = [boss_pos, (startx_mac, starty_mac),]
 
     def random_pos(self, ):
-        while self.maze.structure[self.tile_y_needle][self.tile_x_needle] != "0":
+        while self.maze.structure[self.tile_y_needle][self.tile_x_needle] != "0"\
+            or (self.tile_x_needle,self.tile_y_needle) in self.forbiden_tulpes:
             self.tile_x_needle = random.randint(0, 14)
             self.x_needle = self.tile_x_needle * sprite_size
             self.tile_y_needle = random.randint(0, 14)
             self.y_needle = self.tile_y_needle * sprite_size
+        self.forbiden_tulpes.append((self.tile_x_needle,self.tile_y_needle))
         while self.maze.structure[self.tile_y_ether][self.tile_x_ether] != "0":
             self.tile_x_ether = random.randint(0, 14)
             self.x_ether = self.tile_x_ether * sprite_size
             self.tile_y_ether = random.randint(0, 14)
             self.y_ether = self.tile_y_ether * sprite_size
-            print(self.x_ether)
+        self.forbiden_tulpes.append((self.tile_x_ether,self.tile_y_ether))
         while self.maze.structure[self.tile_y_tube][self.tile_x_tube] != "0":
             self.tile_x_tube = random.randint(0, 14)
             self.x_tube = self.tile_x_tube * sprite_size
             self.tile_y_tube = random.randint(0, 14)
             self.y_tube = self.tile_y_tube * sprite_size
-            print(self.x_tube)
+        self.forbiden_tulpes.append((self.tile_x_tube,self.tile_y_tube))
--- macgyver.py
+++ macgyver.py
@@ -1,10 +1,12 @@
 import pygame
+import time
 from pygame.locals import *
 from level import Level
 from character import Character
 from item import Items
 from constantes import *
 
+pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
 maze = Level()
 maze.generate()
@@ -23,7 +25,11 @@ ether = pygame.transform.scale(pygame.image.load(
     "img/ether.png").convert_alpha(), (sprite_size, sprite_size))
 tube = pygame.transform.scale(pygame.image.load(
     "img/tube.png").convert_alpha(), (sprite_size, sprite_size))
-print((item.x_needle, item.y_needle))
+
+sound_step = pygame.mixer.Sound("sound/footstep.wav")
+sound_item = pygame.mixer.Sound("sound/item.ogg")
+sound_gameover = pygame.mixer.Sound("sound/gameover.wav")
+sound_win = pygame.mixer.Sound("sound/win.wav")
 
 while game:
 
@@ -34,32 +40,42 @@ while game:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 mac.move("right")
+                sound_step.play()
             if event.key == K_LEFT:
                 mac.move("left")
+                sound_step.play()
             if event.key == K_UP:
                 mac.move("up")
+                sound_step.play()
             if event.key == K_DOWN:
                 mac.move("down")
+                sound_step.play()
 
     # if mac are in the same pos than the boss
     # print((mac.x, mac.y))
     if (mac.x, mac.y) == (item.x_ether, item.y_ether):
         item.x_ether, item.y_ether = 0, 0
         item_count += 1
+        sound_item.play()
     if (mac.x, mac.y) == (item.x_tube, item.y_tube):
         item.x_tube, item.y_tube = 0, 0
         item_count += 1
+        sound_item.play()
     if (mac.x, mac.y) == (item.x_needle, item.y_needle):
         item.x_needle, item.y_needle = 0, 0
         item_count += 1
+        sound_item.play()
     if (mac.x, mac.y) == boss_pos:
         if item_count < 3:
-            game = 0
+            sound_gameover.play()
             print("game over")
+            time.sleep(1)
+            game = 0
         if item_count == 3:
-            game =0
+            sound_win.play()
             print("gagne")
-
+            time.sleep(1)
+            game =0
     maze.display(windows)
     windows.blit(boss, boss_pos)
     windows.blit(needle, (item.x_needle, item.y_needle))
