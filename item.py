9c491974476fe0e4f717522feb2214d875f0c30d
hugo <sirso.fell@protonmail.com>
2019-05-07 09:28:02 +0200

    qdding requirements pip freeze

--- item.py
+++ item.py
@@ -32,13 +32,15 @@ class Items(object):
             self.tile_y_needle = random.randint(0, 14)
             self.y_needle = self.tile_y_needle * sprite_size
         self.forbiden_tulpes.append((self.tile_x_needle,self.tile_y_needle))
-        while self.maze.structure[self.tile_y_ether][self.tile_x_ether] != "0":
+        while self.maze.structure[self.tile_y_ether][self.tile_x_ether] != "0"\
+            or (self.tile_x_ether,self.tile_y_ether) in self.forbiden_tulpes:
             self.tile_x_ether = random.randint(0, 14)
             self.x_ether = self.tile_x_ether * sprite_size
             self.tile_y_ether = random.randint(0, 14)
             self.y_ether = self.tile_y_ether * sprite_size
         self.forbiden_tulpes.append((self.tile_x_ether,self.tile_y_ether))
-        while self.maze.structure[self.tile_y_tube][self.tile_x_tube] != "0":
+        while self.maze.structure[self.tile_y_tube][self.tile_x_tube] != "0"\
+            or (self.tile_x_tube,self.tile_y_tube) in self.forbiden_tulpes:
             self.tile_x_tube = random.randint(0, 14)
             self.x_tube = self.tile_x_tube * sprite_size
             self.tile_y_tube = random.randint(0, 14)
--- macgyver.py
+++ macgyver.py
@@ -26,6 +26,7 @@ ether = pygame.transform.scale(pygame.image.load(
 tube = pygame.transform.scale(pygame.image.load(
     "img/tube.png").convert_alpha(), (sprite_size, sprite_size))
 
+#loading sound
 sound_step = pygame.mixer.Sound("sound/footstep.wav")
 sound_item = pygame.mixer.Sound("sound/item.ogg")
 sound_gameover = pygame.mixer.Sound("sound/gameover.wav")
@@ -50,19 +51,16 @@ while game:
             if event.key == K_DOWN:
                 mac.move("down")
                 sound_step.play()
-
-    # if mac are in the same pos than the boss
-    # print((mac.x, mac.y))
     if (mac.x, mac.y) == (item.x_ether, item.y_ether):
         item.x_ether, item.y_ether = 0, 0
         item_count += 1
         sound_item.play()
     if (mac.x, mac.y) == (item.x_tube, item.y_tube):
-        item.x_tube, item.y_tube = 0, 0
+        item.x_tube, item.y_tube = 0, 40
         item_count += 1
         sound_item.play()
     if (mac.x, mac.y) == (item.x_needle, item.y_needle):
-        item.x_needle, item.y_needle = 0, 0
+        item.x_needle, item.y_needle = 0, 80
         item_count += 1
         sound_item.play()
     if (mac.x, mac.y) == boss_pos:
